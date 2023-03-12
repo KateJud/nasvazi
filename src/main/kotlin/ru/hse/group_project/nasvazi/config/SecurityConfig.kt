87b8bb5bb5ee894ff9cbd239746da8bf5df63236
alf3ratz <apetropavlovskij@yandex.ru>
2023-03-12 18:43:32 +0300

    Edit gradle-build

--- src/main/kotlin/ru/hse/group_project/nasvazi/config/SecurityConfig.kt
+++ src/main/kotlin/ru/hse/group_project/nasvazi/config/SecurityConfig.kt
@@ -1,10 +1,10 @@
 package ru.hse.group_project.nasvazi.config
 
-import org.springframework.security.core.context.SecurityContextHolder
-
-// @Configuration
-// open class SecurityConfig : GlobalMethodSecurityConfiguration() {
-// }
-
-val currentUser: String
-    get() = (SecurityContextHolder.getContext()?.authentication?.principal as String?) ?: "TEST"
+//import org.springframework.security.core.context.SecurityContextHolder
+//
+//// @Configuration
+//// open class SecurityConfig : GlobalMethodSecurityConfiguration() {
+//// }
+//
+//val currentUser: String
+//    get() = (SecurityContextHolder.getContext()?.authentication?.principal as String?) ?: "TEST"
